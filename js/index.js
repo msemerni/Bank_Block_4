8dc4c258c5faf50b9a9c785ca32f1f0831b7f6f1
msemerni <msemerni@gmail.com>
2022-02-22 14:20:22 +0200

    block4

--- js/index.js
+++ js/index.js
@@ -12,12 +12,11 @@
 // 1. Подсчитать общее количество денег внутри банка в долларовом эквиваленте учитывая кредитные лимиты и снятие средств. 
 // 2. Посчитать сколько всего денег в долларовом эквиваленте все клиенты должны банку. 
 // 3. Посчитать сколько неактивных клиентов должны погасить кредит банку и на какую общую сумму. 
-// 4. Аналогично для активных. 
+// 3b. Аналогично для активных. 
 
 // Для получения актуальных курсов валют использовать API (https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5). 
 // Промисы использовать для работы с API в целях отправки запросов на сервер.
 // Создать отдельный git-репозиторий для этого проекта и дальше работать с этим проектом в этом репозитории.
-
 
 class Client {
   static id = 1;
@@ -34,7 +33,7 @@ class Client {
   }
 
   addDebitAccount(debitBalance, expiredDate, currencyType) {
-    if (debitBalance >= 0) {
+    if(debitBalance >= 0) {
       this.debitAccounts.push({
         isActive: true,
         debitBalance: debitBalance,
@@ -47,7 +46,7 @@ class Client {
   }
 
   addCreditAccount(creditLimit, expiredDate, currencyType) {
-    if (creditLimit >= 0) {
+    if(creditLimit >= 0) {
       this.creditAccounts.push({
         isActive: true,
         creditBalance: creditLimit,
@@ -61,114 +60,69 @@ class Client {
   }
 }
 
-let misha = new Client("Misha", true, new Date(2011, 0, 1));
-misha.addDebitAccount(1000, new Date(2022, 11, 31), "UAH");
-misha.addDebitAccount(200, new Date(2024, 7, 15), "EUR");
+// let misha = new Client("Misha", true, new Date(2011, 0, 1));
+// misha.addDebitAccount(1000, new Date(2022, 11, 31), "UAH");
+// misha.addDebitAccount(200, new Date(2024, 7, 15), "EUR");
+// misha.addCreditAccount(3500, new Date(2025, 8, 5), "UAH");
+// misha.addCreditAccount(100, new Date(2022, 8, 5), "USD");
 
-misha.addCreditAccount(3500, new Date(2025, 8, 5), "UAH");
-misha.addCreditAccount(100, new Date(2022, 8, 5), "USD");
-// misha.creditAccounts[1].creditBalance -= 50;
-
-
-
-
-let ira = new Client("Ira", false, new Date(2020, 7, 9));
-ira.addDebitAccount(500, new Date(2022, 5, 20), "USD");
-// ira.addDebitAccount(600, new Date(2023, 6, 25), "EUR");
-
-ira.addCreditAccount(200, new Date(2026, 4, 2), "EUR");
-// ira.creditAccounts[0].creditBalance -= 150;
-
-console.log(misha);
-console.log(ira);
-console.log(Client.clientBase);
-console.log(misha.debitAccounts[0].currencyType);
-
-ira.isActive = true;
-// console.log(ira);
-
+// let ira = new Client("Ira", false, new Date(2020, 7, 9));
+// ira.addDebitAccount(500, new Date(2022, 5, 20), "USD");
+// ira.addCreditAccount(200, new Date(2026, 4, 2), "EUR");
 
 function getCurrencyRates() {
   let currencyRates = fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
-  .then(response => response.json());
-  // .then(data => console.log(data));
+    .then(response => response.json());
   return currencyRates;
 }
-
 
 function getBankUSDAmount() {
   let sumUSD = 0;
 
   getCurrencyRates()
     .then(rates => {
-      console.log(rates);
       Client.clientBase.map(client => {
-        // console.log(client);
-        
         client.creditAccounts.map(creditAccounts => {
-          // console.log(creditBal.currencyType);
           let usdRate;
 
-          for (let i = 0; i < rates.length; i++) {
-            if (rates[i].ccy === "USD") {
+          for(let i = 0; i < rates.length; i++) {
+            if(rates[i].ccy === "USD") {
               usdRate = rates[i].sale;
-              console.log(usdRate + " USD SALE");
             }
 
-            if (creditAccounts.currencyType === "UAH") {
+            if(creditAccounts.currencyType === "UAH") {
               sumUSD += (creditAccounts.creditBalance / usdRate);
-              console.log(sumUSD + " UAH to USD");
               return;
             }
 
-            if (creditAccounts.currencyType === rates[i].ccy) {
-              // console.log(rates[i].ccy + "rate" + rates[i].sale);
-              console.log(creditAccounts);
+            if(creditAccounts.currencyType === rates[i].ccy) {
               sumUSD += (creditAccounts.creditBalance * (rates[i].sale / usdRate));
-              console.log(rates[i].ccy + " TO usd: " + rates[i].sale / usdRate);
             }
           }
         });
-//////////////////////
 
         client.debitAccounts.map(debitAccounts => {
-          // console.log(creditBal.currencyType);
           let usdRate;
 
-          for (let i = 0; i < rates.length; i++) {
-            if (rates[i].ccy === "USD") {
+          for(let i = 0; i < rates.length; i++) {
+            if(rates[i].ccy === "USD") {
               usdRate = rates[i].sale;
-              console.log(usdRate + " USD SALE");
             }
 
-            if (debitAccounts.currencyType === "UAH") {
+            if(debitAccounts.currencyType === "UAH") {
               sumUSD += (debitAccounts.debitBalance / usdRate);
-              console.log(sumUSD + " UAH to USD");
               return;
             }
 
-            if (debitAccounts.currencyType === rates[i].ccy) {
-              // console.log(rates[i].ccy + "rate" + rates[i].sale);
-              console.log(debitAccounts);
+            if(debitAccounts.currencyType === rates[i].ccy) {
               sumUSD += (debitAccounts.debitBalance * (rates[i].sale / usdRate));
-              console.log(rates[i].ccy + " TO usd: " + rates[i].sale / usdRate);
             }
           }
         });
-
-/////////////////////
-
-
-
       });
-
-      console.log("Total USD in Bank: " + sumUSD);
+      // console.log("Total USD in Bank: " + sumUSD);
       return sumUSD;
     });
 }
 
-
-
-let dd = getBankUSDAmount();
-console.log(dd + " UAH to USD444");
-
+getBankUSDAmount();
